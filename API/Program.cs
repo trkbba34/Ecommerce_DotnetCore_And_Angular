c32416b454e57037e36e85038bfc56fb5f8c2f3f
tarek chenafi <tarekchenafi@gmail.com>
2024-09-21 03:15:52 +0100

    S07

--- API/Program.cs
+++ API/Program.cs
@@ -23,7 +23,7 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
-    .WithOrigins("http://localhost/4200", "https://localhost/4200"));
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
